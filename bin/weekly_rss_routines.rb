43d40c4d398fec3bf76a7b4be544c147e52a3dc1
MSAMohammad <36763326+MSAMohammad@users.noreply.github.com>
2018-02-23 14:31:05 +0200

    Update weekly_rss_routines.rb

--- bin/weekly_rss_routines.rb
+++ bin/weekly_rss_routines.rb
@@ -1,87 +1,94 @@
-#!/usr/bin/env ruby
-# == Synopsis
-#   run the daily rss feeds
-# == Usage
-#  daily_rss_routines.rb  -h host -p port -d " 
-# == Useful commands
-# daily_rss_routines.rb  -d 
-# daily_rss_routines.rb  -h svbalance.cure.com.ph
-# == Author
-#   Scott Sproule  --- Ficonab.com (scott.sproule@ficonab.com)
-# == Copyright
-#    Copyright (c) 2007 Ficonab Pte. Ltd.
-#     See license for license details
-require 'rubygems'
-require 'optparse'
-#
-begin
-  gem 'simple_sms_services'
-  require 'simple_sms_services'
-rescue LoadError => e
-  puts '---- simple sms services required'
-  puts '---- install with the following command'
-  puts '----- sudo gem install simple_sms_services'
-  puts "----- #{e.backtrace}"
-  exit(1)
-end
-require 'optparse'
-gem 'stomp_message'
-require 'stomp_message'
-require 'rdoc/usage'
+Project 3: Clinic appointment system
 
- arg_hash=StompMessage::Options.parse_options(ARGV)
- RDoc::usage if   arg_hash[:help]==true
-require 'pp'
+1.	Justify why you have selected this project.
+
+I select this project because I have a good background in class UML (object-oriented programming) and I interesting using it in my works. Also, this project is very useful in our live especially in Palestine because the Palestinian hospitals and clinics have a weak systems.  
+
+2.	Explain why your approach best fits the problem.
+
+I think my approach is the best fits the problem because I had cover all the requirements which is needed. Also, because its simple and don’t want complicated processes.  
+
+3.	The testing scenarios.
+
+•	Check Patient register for an appointment.
+•	Check Patient want to cancel or delay the appointment.
+•	Check doctor or clinic send the acceptance for the patient canceling or delaying.
+•	Check doctor or clinic want to cancel or delay the appointment.
+
+
+4.	How to run the code on windows or Mac if applicable.
+
+NO code.
+
+5.	Optimization techniques, if any.
+
+None
 
 
 
-to_do = { 
-           'grandprixhardcore' => 'http://www.grandprix.com/ft/rss.xml',
-             'grandprix' => 'http://www.grandprix.com/rss.xml',
-             'winespec' => 'http://www.winespectator.com/RSS/headlines_rss/0,3991,,00.xml'
-        #   'weather' => 'http://www.weather.com/weather/rss/subscription/RPXX0017'
-         }
-feed_messages = []
-to_do.each { |keyword, url|   puts "Processing #{keyword}: from: #{url}"
-                #arguments = " -U #{url} -t #{keyword} --k #{keyword} "
-                 feed=SimpleSmsServices::Feed.new(url,keyword)
-                 args = {}
-                 args=args.merge(arg_hash)
-               #  puts "args is #{args}  arg hash is #{arg_hash}"
-                 args[:broadcast]=""
-                 args[:keyword]=keyword
-                 result=feed.send_sms_to_subscription(args)
-                 feed_messages << result
-                 puts "result is #{result}"
-                 feed=nil
-                 sleep(1)  #just to let smsc relax
-              } if false  # do not do this now...
-to_do.each { |keyword, url|   puts "RSS text Processing #{keyword}: from: #{url}"
-                              #arguments = " -U #{url} -t #{keyword} --k #{keyword} "
-                               feed=SimpleSmsServices::Feed.new(url,keyword)
-                               feed.unsubscribe_command="subscribe #{keyword} off"
-                               args = {}
-                               args=args.merge(arg_hash)
-                             #  puts "args is #{args}  arg hash is #{arg_hash}"
-                               args[:broadcast]=""
-                               args[:keyword]=keyword
-                               result=feed.send_mms_to_subscription(args)
-                               feed_messages << result
-                               puts "RSS result is #{result}"
-                               feed=nil
-                               sleep(1)  #just to let smsc relax
-                            }              
-puts "Result is #{feed_messages.join("\n")}"
-  email='scott.sproule@cure.com.ph'
-                
-                    email='scott.sproule@cure.com.ph'
-                    StompMessage::StompSendTopic.send_email_stomp("scott.sproule@cure.com.ph","WEEKLY RSS", email,
-                                    "rss feeds: processed  #{feed_messages.size}", feed_messages.join('\n'))
 
-#{}`get_feed.rb  -U 'http://newsrss.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml' -t bbcnews --k bbcnews `
-#puts "iht news"
-#{}`get_feed.rb  -U 'http://www.iht.com/rss/frontpage.xml' -t IHT -k ihtnews`
-#puts "bbc sports"
-# `get_feed.rb  -U 'http://www.iht.com/rss/frontpage.xml' -t IHT -k ihtnews`
-exit!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+6.	A workflow diagram showing the logical flow of the solution. [This is needed to give the reviewers the chance to track and trace the solution]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+7.	A UML diagram, if applicable.
+
+ 
+
 
