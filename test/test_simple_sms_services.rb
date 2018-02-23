c66aa3d5e62792046fded5c52738f0504fb76565
MSAMohammad <36763326+MSAMohammad@users.noreply.github.com>
2018-02-23 14:29:30 +0200

    Update test_simple_sms_services.rb

--- test/test_simple_sms_services.rb
+++ test/test_simple_sms_services.rb
@@ -1,85 +1,94 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+Project 3: Clinic appointment system
 
-class TestSimpleSmsServices < Test::Unit::TestCase
+1.	Justify why you have selected this project.
 
-  def setup
-  end
-  
-  def test_flickr
-       feed=SimpleSmsServices::FlickrImages.new("sexy girl",'flickr photos')
-       data = feed.process
-       puts "found #{data} pictures using sexy girl"
-       assert data > 0, "no pictures found"
-       feed.send_to_mms_manager({}, '639993130030')
-      # feed.send_to_mms_manager({}, '639993130313')
-     #  assert data.include?("title_by_scott"), "title wrong"
-   end
-   def test_flickr_bad_tag
-         feed=SimpleSmsServices::FlickrImages.new("abc de234 zw34",'flickr bad tag')
-         data = feed.process
-         puts "found #{data} pictures using abc de234 zw34 "
-         assert data == 0, " pictures found with bad tag"
-         feed.send_to_mms_manager({}, '639993130030')
-        # feed.send_to_mms_manager({}, '639993130313')
-       #  assert data.include?("title_by_scott"), "title wrong"
-     end
-   def test_flickr_subscription
-        feed=SimpleSmsServices::FlickrImages.new("bikini",'title_by_scott')
-        data = feed.process
-        puts "found #{data} pictures"
-        assert data > 0, "no pictures found"
-          arg_hash={}
-         arg_hash[:keyword] = 'flickr_bikini'
-        feed.send_mms_to_subscription(arg_hash)
-      #  assert data.include?("title_by_scott"), "title wrong"
-    end
-     def test_flickr_process
-          arg_hash={}
-          subscriptions=SimpleSmsServices::ProcessSubscriptions.new("flickr",arg_hash)
-          data=subscriptions.subscriptions
-          puts "found #{data.size} subscriptions"
-          assert data.size > 0, "no subs found"
-    
-        #  assert data.include?("title_by_scott"), "title wrong"
-      end
-       def test_email_test
-            arg_hash={}
-            email=SimpleSmsServices::EmailToMMS.new('pop.gmail.com','995',
-                       'test@ficonab.com','321test123','639993130030',arg_hash)
-            
-            
+I select this project because I have a good background in class UML (object-oriented programming) and I interesting using it in my works. Also, this project is very useful in our live especially in Palestine because the Palestinian hospitals and clinics have a weak systems.  
 
-          #  assert data.include?("title_by_scott"), "title wrong"
-        end
-  
-  def test_feed
-      feed=SimpleSmsServices::Feed.new( "http://www.iht.com/rss/frontpage.xml",'title_by_scott')
-      data = feed.get_data
-      assert data.include?("http"), "no http in message"
-      assert data.include?("title_by_scott"), "title wrong"
-  end
-  def test_feed_mms
-       feed=SimpleSmsServices::Feed.new( "http://www.iht.com/rss/frontpage.xml",'title_by_scott')
-       data = feed.get_data_mms
-       puts "data is #{data}"
-       assert data.include?("http"), "no http in message"
-       assert data.include?("title_by_scott"), "title wrong"
-   end
-  def test_data_manip
-       feed=SimpleSmsServices::Feed.new( "http://www.iht.com/rss/frontpage.xml",'title_by_scott')
-       data = feed.get_data
-       d=feed.filter_data(d)
-       assert data!=d, "data after filter is same possble but unlikely"
-        d=feed.filter_data("this is '  bad [mov]")
-        assert !d.include?('quot'), "data after filter is wrong"
-       d=feed.filter_data("this is bad [mov]")
-       assert !d.include?('['), "data after filter is wrong"
-       d=feed.filter_data('this is bad " ')
-       assert !d.include?('['), "data after filter is wrong" 
-       assert !d.include?('quot'), "data after filter is wrong" 
-       assert !d.include?('amp'), "data after filter is wrong"    
-         d=feed.filter_data('bad_underscore " ')
-          assert !d.include?('_'), "data after filter is wrong"      
-      # assert data.include?("title_by_scott"), "title wrong"
-   end
-end
+2.	Explain why your approach best fits the problem.
+
+I think my approach is the best fits the problem because I had cover all the requirements which is needed. Also, because its simple and don’t want complicated processes.  
+
+3.	The testing scenarios.
+
+•	Check Patient register for an appointment.
+•	Check Patient want to cancel or delay the appointment.
+•	Check doctor or clinic send the acceptance for the patient canceling or delaying.
+•	Check doctor or clinic want to cancel or delay the appointment.
+
+
+4.	How to run the code on windows or Mac if applicable.
+
+NO code.
+
+5.	Optimization techniques, if any.
+
+None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+6.	A workflow diagram showing the logical flow of the solution. [This is needed to give the reviewers the chance to track and trace the solution]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+7.	A UML diagram, if applicable.
+
+ 
+
+
